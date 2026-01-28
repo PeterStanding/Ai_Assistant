cfea5703baf4812b9d171951b5cb32b315bd16bf
Peter Standing <44238507+PeterStanding@users.noreply.github.com>
2026-01-28 15:56:26 +0000

    Started the outline for the reminders

---
+++ reminders.py
@@ -0,0 +1,20 @@
+# File to aid in the tracking of Reminders
+# Need functions to add reminders, delete reminders, ask for upcoming reminders and any reminders today
+
+from datetime import date
+
+# Expenses file Location
+location = "text_files/reminders.txt"
+dictionary = {}
+today = date.today()
+'''
+def add_reminder():
+def delete_reminder():
+def upcoming_reminders():
+def reminders_today():
+def reminders_month():
+'''
+
+print("Current Date:", today.day)
+print("Current Month:",today.month)
+print("Current Year:",today.year)
